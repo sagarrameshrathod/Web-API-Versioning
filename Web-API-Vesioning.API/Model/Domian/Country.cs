815e347667c64eb2f107ea2b9d244b32b27843cb
Sagar Rathod <sagarrathod.srr21@gmail.com>
2025-06-10 23:19:41 +0530

    Added Versioning Controller

---
+++ Web-API-Vesioning.API/Model/Domian/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_API_Vesioning.API.Model.Domian
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Web-API-Vesioning.API/V2/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Vesioning.API.Model.DTOs;
+
+namespace Web_API_Vesioning.API.V2.Controllers
+{
+    [Route("api/v2/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var contriesDomailModel = CountriesData.Get();
+            var response = new List<CountryDtoV2>();
+            foreach (var countryDomain in contriesDomailModel)
+            {
+                response.Add(new CountryDtoV2
+                {
+                    Id = countryDomain.Id,
+                    CountryName = countryDomain.Name,
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
